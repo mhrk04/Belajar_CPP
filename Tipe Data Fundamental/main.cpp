9cfc516df28a5a7e9a105826ee54ee957dabb5a7
mhrk04 <haziqrohaizan@gmail.com>
2022-08-05 23:04:21 +0800

    data type

---
+++ Tipe Data Fundamental/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  // put unsigned for more int without +/-
+  unsigned int a = 1;
+  // print nilai a
+
+  cout<< a<< endl;
+  // boolean
+  bool datang= false;
+  // long 64bit
+  cout<< datang;
+// show size integer in byte
+  cout<<sizeof(a)<< "byte"<<endl;
+  // limit num can use in C++
+  // cout<<numeric_limits<int>::max() <<endl;
+  // int max : +/- 2^31 positif mula dari 0, negatif boleh .....sampai 8
+  cin.get();
+  return 0;
+} // namespace std
+
